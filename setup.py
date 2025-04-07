3d0d9ad687b8a1c1553d942200f555d155c094b5
VasilisSz <basilis.sz@hotmail.com>
2025-04-07 12:35:55 +0200

    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 print(find_packages())
 setup(
     name="epoch_tools",
-    version="0.1.1",
+    version="0.1.2",
     author="Vasilis Siozos",
     author_email="v.siozos@rug.nl",
     description="A package for analyzing and clustering EEG epochs",
